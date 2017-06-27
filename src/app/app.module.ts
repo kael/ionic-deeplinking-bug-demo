f5ca3402fb92f3bfd7bd55d7d2a16607cc08ab5c
kael <ka-el@laposte.net>
2017-06-27 18:11:08 +0200

    [navigation] @IonicPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NavigationPageModule } from '../pages/navigation/navigation.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NavigationPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/pages/navigation/navigation.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the NavigationPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage({
+  name: 'navigation'
+})
+@Component({
+  selector: 'page-navigation',
+  templateUrl: 'navigation.html',
+})
+export class NavigationPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NavigationPage');
+  }
+
+}
